385b3cf68745b4260af79cc4b3eab6e51d99eb20
Shraddhanand <1shraddhanand@gmail.com>
2023-03-31 22:52:07 +0530

    SS

--- job-portal-panel/client/src/services/helper.js
+++ job-portal-panel/client/src/services/helper.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:4000"
-// export const BASE_URL = "https://mernappbackend.onrender.com"
+export const BASE_URL = "https://job-portal-57nx.onrender.com"
+// export const BASE_URL = "http://localhost:4000"
